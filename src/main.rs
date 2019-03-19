961593b5bf65b147635aa2c4fef546fc5014adec
corvids <joonnifer@gmail.com>
2019-03-18 21:03:27 -0700

    simple server establishes connection on localhost:4000

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    establish_connection();
+}
+
+fn establish_connection() {
+    let listener = TcpListener::bind("127.0.0.1:4000").unwrap();
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established at 127.0.0.1:4000");
+    }
 }
